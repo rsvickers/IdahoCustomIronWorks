015531df1edde04bcb43848ac899d1ed1cfb1e0c
Rhys Vickers <141367198+rsvickers@users.noreply.github.com>
2024-04-18 08:43:05 -0600

    push

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -5,14 +5,35 @@
 <template>
   <div class="container-fluid">
     <section class="row heroImg box">
-      <div class="col-12 text-light box rounded p-2 mt-3 text-center">
+      <div class="col-12 text-light box rounded p-2 mt-3 text-center text-shadow">
         <h1 class="">Idaho Custom Iron Works</h1>
-        <h5 class="mt-5">We’re the company that exceeds the clients expectations. Idaho Custom Iron Works is locally
+        <h5 class="mt-5 p-2">We’re the company that exceeds the clients expectations. Idaho Custom Iron Works is locally
           owned,
           family operated and licensed in both Idaho and Oregon. We have over 15 years experience doing Commercial and
           Residential Metal Fabrication and install. </h5>
       </div>
     </section>
+
+    <section class="row mt-4 mb-2">
+      <div class="col-12 col-md-7 text-center">
+        <img class="img-fluid"
+          src="https://idahocustomironworks.com/wp-content/uploads/2021/08/UL-Logo-2020-Horizontal-300x59.png"
+          alt="UltraLox Interlocking Technology">
+        <p class="mt-3">Almost two decades of commercial and residential custom iron works. The Treasure Valley has a
+          new technology now available called ULTRALOX INTERLOCKING® TECHNOLOGY, premium powder-coated aluminum picket
+          and glass railing systems for decks, patios and swimming pools with low-maintenance living in mind. Our
+          dedication to research and development ensures a uniquely engineered railing system that meets
+          commercial/residential building codes throughout the US.</p>
+        <p class="mt-3"> Heyyy</p>
+        <p class="mt-3"></p>
+      </div>
+      <div class="col-12 col-md-5">
+        <img class="img-fluid"
+          src="http://idahocustomironworks.com/wp-content/uploads/2020/07/IDAHO-CUSTOM-IRON-WORKS-300x142.jpg" alt="">
+      </div>
+    </section>
+
+
   </div>
 </template>
 
@@ -31,5 +52,9 @@
   max-height: 100vh;
   align-items: center;
   box-shadow: 0px 5px 20px black;
+}
+
+.text-shadow {
+  text-shadow: 2px 2px 4px black;
 }
 </style>
