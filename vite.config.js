19d76c2255fd33e676d5959c61b5509d4adf3738
Rhys Vickers <141367198+rsvickers@users.noreply.github.com>
2024-05-30 15:58:28 -0600

    meh

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,6 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  build: {
-    outDir: 'docs',
-    sourcemap: false
-  },
   base: '/IdahoCustomIronWorks/',
   server: {
     port: 8080
