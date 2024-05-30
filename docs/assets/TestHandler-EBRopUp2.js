6df7325d1cbc8893e043dd47218bafcb9c2c479c
Rhys Vickers <141367198+rsvickers@users.noreply.github.com>
2024-05-30 14:32:31 -0600

    push

--- docs/assets/TestHandler-EBRopUp2.js
+++
@@ -1,1 +0,0 @@
-import{S as s}from"./index-NQ0bFi6N.js";class t extends s{constructor(){super(),this.on("IS_TESTED",this.onTest)}onTest(e){console.log("Recieved IS_TESTED",e)}}const r=new t;export{r as testHandler};
