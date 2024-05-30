ba9a3d80cd69c592a905237269263e3a9d2fd05e
rsvickers <rs.vickers5@gmail.com>
2024-05-30 22:26:55 +0000

    Update from https://github.com/rsvickers/IdahoCustomIronWorks/commit/c4d24aaf90758c3da06929be45aa1b299d7cd916

---
+++ assets/TestHandler-CZfCw4Lp.js
@@ -0,0 +1,1 @@
+import{S as s}from"./index-BU99MqRS.js";class t extends s{constructor(){super(),this.on("IS_TESTED",this.onTest)}onTest(e){console.log("Recieved IS_TESTED",e)}}const r=new t;export{r as testHandler};
--- assets/TestHandler-CxzvNJfw.js
+++
@@ -1,1 +0,0 @@
-import{S as s}from"./index-Ogx2cC3V.js";class t extends s{constructor(){super(),this.on("IS_TESTED",this.onTest)}onTest(e){console.log("Recieved IS_TESTED",e)}}const r=new t;export{r as testHandler};
